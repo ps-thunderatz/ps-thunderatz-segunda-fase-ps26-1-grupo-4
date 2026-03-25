e68033b131b54ba6c6a9ec0b9b159820220de018
cauag <marinhogodinhocaua@gmail.com>
2026-03-25 20:22:05 -0300

    a

--- src/controller.cpp
+++ src/controller.cpp
@@ -11,22 +11,74 @@
 #include "mcu.hpp"
 #include "controller.hpp"
 
-Controller::Controller() {
-    // TODO: Adicionar a lógica de construção do objeto
+Controller::Controller(Led led, Locomotion locomotion, Rc rc)
+    : led(led), locomotion(locomotion), rc(rc) {}
+
+void Controller::init() {
+    this->current_state = INIT;
+    this->current_level = LEVEL_0;
+    this->turn = STOPPED;
 }
 
 void Controller::run() {
     switch (this->current_state) {
+        case INIT: {
+            // Exemplo de debug:
+            led.on();
+
+            this->current_state = STRATEGY_CHOOSER;
+            break;
+        }
+
         case STRATEGY_CHOOSER: {
-            // TODO: Implementar a lógica de escolha de estratégia
+
+            int x = 0;
+            int y = 0;
+
+            if (x < -50) {
+                this->current_level = LEVEL_0;
+            }
+            else if (y > 50) {
+                this->current_level = LEVEL_1;
+            }
+            else if (x > 50) {
+                this->current_level = LEVEL_2;
+            }
+            else if (y < -50) {
+                this->current_level = LEVEL_3;
+            }
+
+            bool start_command = false;
+
+            if (start_command) {
+                this->current_state = RUN;
+            }
+
+            move_robot(STOPPED);
             break;
         }
+
         case RUN: {
-            // TODO: Implementar a lógica de execução da estratégia
+            int x = 0;
+            int y = 0;
+
+            if (x == -100 && y == -100) {
+                move_robot(BACKWARD);
+                break;
+            }
+
+            strategy_run();
             break;
         }
+
+        case STOP: {
+            move_robot(STOPPED);
+            break;
+        }
+
         default: {
-            // TODO: Implementar a lógica de estado padrão
+            this->current_state = STOP;
+            move_robot(STOPPED);
             break;
         }
     }
@@ -34,49 +86,94 @@ void Controller::run() {
 
 void Controller::move_robot(Direction direction) {
     switch (direction) {
-        // TODO: Implementar a lógica de movimentação do robô
         case FORWARD: {
+            locomotion.set_speed(70, 70);
             break;
         }
+
         case BACKWARD: {
+
+            locomotion.set_speed(-70, -70);
             break;
         }
+
         case LEFT: {
+
+            locomotion.set_speed(-70, 70);
             break;
         }
+
         case RIGHT: {
+
+            locomotion.set_speed(70, -70);
             break;
         }
+
         case STOPPED: {
+
+            locomotion.stop();
             break;
         }
+
         case RC_INPUT: {
+            int x = 0;
+            int y = 0;
+
+            int left = y + x;
+            int right = y - x;
+
+           if (left > 70) left = 70
+           if ()
+            else {
+                move_robot(STOPPED);
+            }
             break;
         }
+
         default: {
+            locomotion.stop();
             break;
         }
+    }
+}
+
+void Controller::set_next_strategy() {
+    this->current_level++;
+
+    if (this->current_level >= this->max_strategy) {
+        this->current_level = 0;
     }
 }
 
 void Controller::strategy_run() {
     switch (this->current_level) {
         case LEVEL_0: {
-            // TODO: Implementar a lógica de execução da estratégia 0
+
+            move_robot(FORWARD);
             break;
         }
+
         case LEVEL_1: {
-            // TODO: Implementar a lógica de execução da estratégia 1
+
+            move_robot(RIGHT);
             break;
         }
+
         case LEVEL_2: {
-            // TODO: Implementar a lógica de execução da estratégia 2
+
+            move_robot(FORWARD);
             break;
         }
+
         case LEVEL_3: {
-            // TODO: Implementar a lógica de execução da estratégia 3
+
+            move_robot(RC_INPUT);
+            break;
         }
+
         default: {
+            move_robot(STOPPED);
+            this->current_state = STOP;
             break;
         }
     }
